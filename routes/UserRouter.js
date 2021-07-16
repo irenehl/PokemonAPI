8ac3c6fe700d0c6a9efe5ba9981715e75b57da46
irenehl <26irenelopez@gmail.com>
2021-07-15 23:51:16 -0600

    🎉 Adding UserController

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,9 +1,14 @@
+const passport = require('passport');
 var express = require('express');
 var router = express.Router();
+var { login, register, createAdmin } = require('../controllers/User/UserController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const options = { session: false }
+
+router.post('/login', passport.authenticate('local', options), login);
+router.post('/register', register);
+
+router.post('/create-admin', passport.authenticate('bearer', options), createAdmin);
+
 
 module.exports = router;
